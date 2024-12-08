d6980fe9ad3391af2c2b5e33f9108b9178739f8d
johnmatter <jmatter4@gmail.com>
2024-12-07 22:23:20 -0500

    add debug output to console

--- lib/util/debug.lua
+++ lib/util/debug.lua
@@ -7,11 +7,15 @@ function debug.log(...)
     str = str .. tostring(v) .. " "
   end
   
+  -- write to debug file
   local file = io.open(_path.data.."norns-render-debug.txt", "a")
   if file then
     file:write(os.date("%Y-%m-%d %H:%M:%S") .. ": " .. str .. "\n")
     file:close()
   end
+
+  -- print to console
+  print(str)
 end
 
 return debug 
